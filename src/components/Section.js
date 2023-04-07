8e3e58c5bd34c3bce700de78fcf3fd646b3b72d6
GustavoGuerato <guerato.gustavo@gmail.com>
2023-04-07 19:42:12 -0300

    creation of section

---
+++ src/components/Section.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section() {
+  return <Wrap></Wrap>;
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: orange;
+`;
