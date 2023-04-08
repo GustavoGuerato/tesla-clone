1673967f354be3899b2989ac567e9d8d66db3352
GustavoGuerato <guerato.gustavo@gmail.com>
2023-04-07 23:40:28 -0300

    customization of header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,50 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a>
+        <img src="/images/logo.svg" alt="" />
+      </a>
+
+      <Menu>
+        <p>
+          <a href="#">Model S</a>
+        </p>
+        <p>
+          <a href="#">Model 3</a>
+        </p>
+        <p>
+          <a href="#">Model X</a>
+        </p>
+        <p>
+          <a href="#">Model Y</a>
+        </p>
+      </Menu>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+
+  p {
+    font-weight: 600;
+    text-transformation: uppercase;
+    padding: 0 10px;
+    flex-wrap: nowrap;
+  }
+`;
