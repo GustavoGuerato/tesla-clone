904c84ced08ae375207b9042f7dd4c5765f7db69
GustavoGuerato <guerato.gustavo@gmail.com>
2023-04-07 22:10:57 -0300

    creation of buttons an styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+
 import "./App.css";
+
+// eslint-disable-next-line no-unused-vars
 import Header from "./components/Header";
 import Home from "./components/Home";
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -2,7 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 function Section() {
-  return <Wrap></Wrap>;
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>Custom order</LeftButton>
+        <RightButton>Existing inventory</RightButton>
+      </ButtonGroup>
+      <DownArrow src="/images/down-arrow.svg" />
+    </Wrap>
+  );
 }
 
 export default Section;
@@ -10,5 +22,44 @@ export default Section;
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background: orange;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 10vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+const RightButton = styled(LeftButton)``;
+
+const DownArrow = styled.img`
+  margin-top: 20px;
+  height: 40px;
 `;
