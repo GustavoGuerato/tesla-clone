9e041d2494c1bd131a3e7157e5380a16cd9a5c0a
GustavoGuerato <guerato.gustavo@gmail.com>
2023-04-10 07:26:09 -0300

    creation of rightmenu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import styled from "styled-components";
-
+import MenuIcon from "@mui/icons-material/Menu";
 function Header() {
   return (
     <Container>
@@ -9,19 +10,20 @@ function Header() {
       </a>
 
       <Menu>
-        <p>
-          <a href="#">Model S</a>
-        </p>
-        <p>
-          <a href="#">Model 3</a>
-        </p>
-        <p>
-          <a href="#">Model X</a>
-        </p>
-        <p>
-          <a href="#">Model Y</a>
-        </p>
+        <a href="#">Model S</a>
+
+        <a href="#">Model 3</a>
+
+        <a href="#">Model X</a>
+
+        <a href="#">Model Y</a>
       </Menu>
+
+      <Rightmenu>
+        <a href="#">Shop</a>
+        <a href="#">Tesla account </a>
+        <CustomMenu />
+      </Rightmenu>
     </Container>
   );
 }
@@ -33,18 +35,41 @@ const Container = styled.div`
   position: fixed;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
 `;
 
 const Menu = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
   flex: 1;
 
-  p {
+  a {
     font-weight: 600;
     text-transformation: uppercase;
     padding: 0 10px;
     flex-wrap: nowrap;
   }
+`;
+const Rightmenu = styled.div`
+  display: flex;
+  align-items: center;
+ 
+  a {
+    font-weight: 600;
+    text-transformation: uppercase;
+    margin-right: 10px;
+  }
+
+  @media (max-width: 768px) {
+    display: none;
+  } ;
+`;
+
+const CustomMenu = styled(MenuIcon)`
+  cursor: pointer;
 `;
